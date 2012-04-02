878ce22989303f57e9d9656d4733234647823f57
Christopher Blum <christopher.blum@xing.com>
2012-04-02 15:55:02 +0200

    Add the state prop to the jquery event object

--- src/event.js
+++ src/event.js
@@ -2,4 +2,7 @@
   // add the dataTransfer property for use with the native 'drag' and 'drop' event
   // to capture information about files dropped into the browser window
   $.event.props.push("dataTransfer");
+  
+  // add the state property for use with the native 'popstate' event
+  $.event.props.push("state");
 })(jQuery);
