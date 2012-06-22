816a2c93cbe7e147ea12f76de9d21256b4673620
tiff <christopher.blum@spellboy.com>
2012-06-22 15:32:28 +0200

    Support for dblclick on iOS

--- src/event.js
+++ src/event.js
@@ -5,4 +5,29 @@
   
   // add the state property for use with the native 'popstate' event
   $.event.props.push("state");
+  
+  var agent = navigator.userAgent.toLowerCase(),
+      isiOS = agent.indexOf("iphone") !== -1 || agent.indexOf("ipad") !== -1 || agent.indexOf('ipod') !== -1;
+  
+  if (isiOS) {
+    $.event.special.dblclick = {
+      setup: function(data) {
+        $.event.add(this, "touchend._dblclick", function(event) {
+          var now       = new Date().getTime(),
+              lastTouch = $.data(event.target, "lastTouch") || now + 1, // the first time this will make delta a negative number
+              delta     = now - lastTouch;
+          if (delta < 500 && delta > 0) {
+            $.event.simulate("dblclick", event.target, event, true);
+          } else {
+            $.event.simulate("mousedown", event.target, event, true);
+          }
+          $.data(event.target, "lastTouch", now);
+        });
+      },
+
+      remove: function(data) {
+        $.event.remove(this, "._dblclick");
+      }
+    };
+  }
 })(jQuery);
